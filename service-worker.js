2d17adba353a0fb96bde0ea8eb6772b3bb0d6625
tanakagagigugego-lang <tanakagagigugego@gmail.com>
2026-04-20 23:22:19 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = "pomodoro-flow-v3";
+const CACHE_NAME = "pomodoro-flow-v4";
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
   "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
@@ -26,9 +28,19 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+      return cachedResponse || fetch(event.request).catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+
+        return Response.error();
+      });
     })
   );
 });
